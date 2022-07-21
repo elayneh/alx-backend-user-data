b94936b29a7550dc740c43ff69c51351934c8af9
elayneh <betenatefera@gmail.com>
2022-07-21 12:05:40 +0300

    update file

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 """ Logging """
 
+from asyncio.log import logger
 import logging
 import os
+from attr import field
 import mysql.connector
 import re
 from typing import List
+
+
+logger = logging.getLogger()
 
 
 def filter_datum(fields: List[str],
@@ -18,3 +23,21 @@ def filter_datum(fields: List[str],
         message = re.sub(fr'{field}=.+?{separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
