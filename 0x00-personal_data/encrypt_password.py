83e9719bc58022ba529ced49b9abef7d4f572fb5
elayneh <betenatefera@gmail.com>
2022-07-21 16:28:01 +0300

    work on encription

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Encryption """
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ Hashed password """
+    password = password.encode('utf-8')
+    hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+    return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Check valid password """
+    password = password.encode('utf-8')
+    if bcrypt.checkpw(password, hashed_password):
+        return True
