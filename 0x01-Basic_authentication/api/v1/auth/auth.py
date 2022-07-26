a431487a849072a76475202f2a6bac6bf4385eb8
elayneh <betenatefera@gmail.com>
2022-07-26 16:38:04 +0300

    task added

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,21 +1,35 @@
 #!/usr/bin/env python3
-""" Class to manage the API authontication """
+""" Auth class, Require auth with stars """
+from flask import request
 from typing import List, TypeVar
-from flask import Flask, request
 
 
 class Auth:
-    """ Class to authentication """
-    
-    def require_auth(self, path: str, excluded_path: List[str]) -> bool:
-        """ Method that implements auth requirements """
-        return False
-    
+    """ manage the API authentication """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require authorithation """
+        if path is None or excluded_paths is None or not len(excluded_paths):
+            return True
+        if path[-1] != '/':
+            path += '/'
+        for i in excluded_paths:
+            if i.endswith('*'):
+                if path.startswith(i[:1]):
+                    return False
+        if path in excluded_paths:
+            return False
+        else:
+            return True
+
     def authorization_header(self, request=None) -> str:
-        """ Method that implements auth header """
-        return request
-    
+        """ authorization header """
+        if request is None:
+            return None
+        if not request.headers.get("Authorization"):
+            return None
+        return request.headers.get("Authorization")
+
     def current_user(self, request=None) -> TypeVar('User'):
-        """ Current user """
-        return request
- 
+        """ current user """
+        return None
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Basic Authontication
+"""
+
+from mysqlx import Auth
+
+
+class BasicAuth(Auth):
+    """ Class that inherits Auth """
+    
