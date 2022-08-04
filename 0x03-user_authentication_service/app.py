384d1b120dd5399001aff68269f9eba8f0a01a56
elayneh <betenatefera@gmail.com>
2022-08-04 11:35:27 +0300

    pycodestyle

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -77,17 +77,17 @@ def logout():
         except ValueError:
             abort(403)
 
-   @app.route('/reset_password', methods=['PUT'])
-def update_password():
-    """ updates the user password """
-    email = request.form.get('email')
-    reset_token = request.form.get('reset_token')
-    new_password = request.form.get('new_password')
-    try:
-        AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"})
-    except Exception as exp:
-        abort(403)
+    @app.route('/reset_password', methods=['PUT'])
+    def update_password():
+        """ updates the user password """
+        email = request.form.get('email')
+        reset_token = request.form.get('reset_token')
+        new_password = request.form.get('new_password')
+        try:
+            AUTH.update_password(reset_token, new_password)
+            return jsonify({"email": email, "message": "Password updated"})
+        except Exception as exp:
+            abort(403)
 
 
 if __name__ == "__main__":
