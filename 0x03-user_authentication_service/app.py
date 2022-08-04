aa7dedf7dedf086a301c71383b40a6eea05a821f
elayneh <betenatefera@gmail.com>
2022-08-04 09:49:10 +0300

    updates

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 """ Application """
-from flask import Flask, jsonify
+
+from crypt import methods
+import email
+from os import abort
+from typing_extensions import Self
+from urllib import response
+from django.shortcuts import redirect
+from flask import Flask, flash, jsonify, make_response
+import flask
+from requests import request, session
+from auth import Auth
+AUTH = Auth()
 
 app = Flask(__name__)
 
@@ -10,6 +21,50 @@ def home():
     """ Home page """
     return jsonify({"message": "Bienvenue"})
 
+
+@app.route('/users', methods=['POST'])
+def users():
+    """ registers a new user """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"})
+
+@app.route('/sessions', methods=['POST'])
+def sessions():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    if not session_id:
+        abort(401)
+    response = make_response(jsonify({"email": email, "message": "logged in "}))
+    response.set_cookie("session_id", session_id)
+    return response
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    """ Delete the session """
+    cookie = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(cookie)
+    if user is None:
+        abort(403)
+    else:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    
+    @app.route('/profile', methods=['GET'])
+    def profile(self):
+        """ Profile """
+        cookie = request.cookies.get('session_id')
+        user = AUTH.get_user_from_session_id(cookie)
+        if user is None:
+            abort(403)
+        else:
+            return jsonify({"email": user.email}), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """ Hashing """
+
 from user import User
 import bcrypt
 from db import DB
+import uuid
 from sqlalchemy.orm.exc import NoResultFound
 
 
@@ -10,6 +12,11 @@ def _hash_password(password: str) -> str:
     """ Password hash """
     hashed_pwd = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hashed_pwd
+
+
+def _generate_uuid() -> str:
+    """ UUID generator """
+    return str(uuid.uuid4())
 
 
 class Auth:
@@ -30,3 +37,39 @@ class Auth:
             hashed_pwd = _hash_password(password)
             user = self._db.add_user(email, hashed_pwd)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Credentials validation """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                return bcrypt.checkpw(password.encode(), user.hashed_password)
+        except NoResultFound:
+            return False
+
+    def create_session(self, email: str) -> str:
+        """ Session creator """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                session_id = _generate_uuid()
+                return session_id
+        except NoResultFound:
+            return None
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """ Return corresponding user if exists """
+
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            if user:
+                return user
+        except Exception:
+            return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """ Destroy session """
+        self._db.update_user(user_id, session_id =None)
+        return None
