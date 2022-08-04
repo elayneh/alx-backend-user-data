b513850e938fca1d88a283b83caa4f159a3a9823
elayneh <betenatefera@gmail.com>
2022-08-04 11:21:02 +0300

    password updattor added

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -87,3 +87,13 @@ class Auth:
             return token
         except Exception as exp:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ Update password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            new_hashed_password = _hash_password(password)
+            self._db.update_user(
+                user.id, hashed_password=new_hashed_password, reset_token=None)
+        except Exception as exp:
+            raise ValueError
